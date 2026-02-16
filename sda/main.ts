1293778e5b9c3b7f3d3304298e50a4fe6eae3d1c
Andrea <a.mussari@studenti.unipi.it>
2026-02-17 00:48:26 +0100

    reset

--- sda/main.ts
+++
@@ -1,8 +0,0 @@
-import { SimpleDB } from "@nshiab/simple-data-analysis";
-import crunchData from "./helpers/crunchData.ts";
-
-const sdb = new SimpleDB({ logDuration: true });
-
-await crunchData(sdb);
-
-await sdb.done();
--- src/components/Highlight.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    let { text, background, color } = $props();
-</script>
-
-<span
-    style={`background-color: ${background}; color: ${color}; padding: 1px 5px; border-radius: 4px; font-weight: 600;`}
->
-    {text}
-</span>
